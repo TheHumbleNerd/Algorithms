662809657428e1e262bb2e9907765f809e8a7c31
Gagan Singhal <gsinghal@adobe.com>
2024-03-14 20:22:57 +0530

    424 solution2

---
+++ Sliding Window/424. Longest Repeating Character Replacement/Solution2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hashMap = {}
+        l = 0
+        longestSubstrLength = 0
+        maxFreq = 0
+        for r in range(len(s)):
+            hashMap[s[r]] = hashMap.get(s[r], 0) + 1
+            maxFreq = max(maxFreq, hashMap[s[r]])
+            if (r - l + 1) - maxFreq > k:
+                hashMap[s[l]] -= 1
+                l += 1
+            longestSubstrLength = max(longestSubstrLength, r-l+1)
+        
+        return longestSubstrLength
