89b64619cc36717f8f76c08b35873d200cf468f0
Gagan Singhal <gsinghal@adobe.com>
2024-03-16 20:44:23 +0530

    567 fixing buggy impl

--- Sliding Window/567. Permutation in String/Solution2.py
+++ Sliding Window/567. Permutation in String/Solution2.py
@@ -9,22 +9,33 @@ class Solution:
         uniqueKeys = len(map1.keys())
         matchingKeys = 0
         map2 = {}
+
         l, r = 0, 0
         while r < windowSize:
             map2[s2[r]] = map2.get(s2[r], 0) + 1
-            if map2[s2[r]] == map1.get(s2[r], 0):
-                matchingKeys += 1
             r += 1
+        for key in map2.keys():
+            if map2[key] == map1.get(key, 0):
+                matchingKeys += 1
+
         if matchingKeys == uniqueKeys:
             return True
+
         while r < len(s2):
-            if map2[s2[l]] == map1.get(s2[l], 0):
-                matchingKeys -= 1
+            if map2[s2[l]] == map1.get(s2[l], -1):
+                    matchingKeys -= 1
             map2[s2[l]] -= 1
-            map2[s2[r]] = map2.get(s2[r], 0) + 1
-            if map2[s2[r]] == map1.get(s2[r], 0):
+            if map2[s2[l]] == map1.get(s2[l], -1):
                 matchingKeys += 1
             l += 1
+
+            matchingBefore = map2.get(s2[r], 0) == map1.get(s2[r], -1)
+            map2[s2[r]] = map2.get(s2[r], 0) + 1
+            if map2[s2[r]] == map1.get(s2[r], -1):
+                    matchingKeys += 1
+            elif matchingBefore and map2[s2[r]] > map1.get(s2[r], 0):
+                matchingKeys -= 1
+            
             r += 1
             if matchingKeys == uniqueKeys:
                 return True
