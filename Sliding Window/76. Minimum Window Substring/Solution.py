7be591a137ff229ca986007000b657448ad7bf57
Gagan Singhal <gsinghal@adobe.com>
2024-03-17 20:51:36 +0530

    76 optimization

--- Sliding Window/76. Minimum Window Substring/Solution.py
+++ Sliding Window/76. Minimum Window Substring/Solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         result = ""
+        resLen = float('infinity')
         map1 = {}
         for i in t:
             map1[i] = map1.get(i, 0) + 1
@@ -10,21 +11,21 @@ class Solution:
         matchingKeys = 0
         l, r = 0, 0
         while r < len(s):
-            while r < len(s) and matchingKeys != uniqueKeys:
-                map2[s[r]] = map2.get(s[r], 0) + 1
-                if map2[s[r]] == map1.get(s[r], -1):
-                    matchingKeys += 1
-                r += 1
+            map2[s[r]] = map2.get(s[r], 0) + 1
+            if s[r] in map1 and map2[s[r]] == map1[s[r]]:
+                matchingKeys += 1
             
-            while l < r and l < len(s) and matchingKeys == uniqueKeys:
-                tempResult = s[l:r]
-                if len(result) == 0 or len(tempResult) < len(result):
-                    result = tempResult
+            while matchingKeys == uniqueKeys:
+                if resLen > r - l + 1:
+                    result = s[l:r+1]
+                    resLen = r - l + 1
                 
-                if map2[s[l]] == map1.get(s[l], -1):
+                if s[l] in map1 and map2[s[l]] == map1[s[l]]:
                     matchingKeys -= 1
                 map2[s[l]] -= 1
 
                 l += 1
-            
+
+            r += 1
+
         return result
