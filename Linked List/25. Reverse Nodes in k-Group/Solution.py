c4095e57a1af51d02b5d173cbcfaf83c0173bbc1
Gagan Singhal <gsinghal@adobe.com>
2024-03-19 21:32:39 +0530

    25 reverse nodes in k group

---
+++ Linked List/25. Reverse Nodes in k-Group/Solution.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getKth(self, s: Optional[ListNode], k: int) -> Optional[ListNode]:
+        for i in range(k-1):
+            if s:
+                s = s.next
+            else:
+                break
+        return s
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        newHead = head
+        groupPrev = None
+        groupStart = head
+        while True:
+            groupLast = self.getKth(groupStart, k)
+            if groupLast is None:
+                break
+
+            groupNext = groupLast.next
+
+            current = groupStart
+            prev = groupLast
+            while current != groupNext:
+                next = current.next
+                current.next = prev
+                prev = current
+                current = next
+            groupStart.next = current
+            
+            if not groupPrev:
+                newHead = prev
+            else:
+                groupPrev.next = groupLast
+
+            groupPrev = groupStart
+            groupStart = current
+
+        return newHead
